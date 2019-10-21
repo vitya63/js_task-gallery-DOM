99605511f6e4c4f69bc9794a14facbfbf12e6b01
Viktor Lukovets <nuerio63@gmail.com>
2019-10-21 15:36:51 +0300

    Updates

--- scripts/main.js
+++ scripts/main.js
@@ -4,7 +4,8 @@ const galleryList = document.querySelector('#thumbs');
 const largeImg = document.querySelector('#largeImg');
 const handler = function(e) {
   e.preventDefault();
-  const imgSrc = e.target.src.replace('-thumb.jpeg', '.png');
-  largeImg.src = imgSrc;
+  const link = e.target.closest('.list-item__link');
+  largeImg.src = link.href;
+  largeImg.alt = link.title;
 };
 galleryList.addEventListener('click', handler);
